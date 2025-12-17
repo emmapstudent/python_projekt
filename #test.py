cb52bd62d8ee254c50e53175b3553b7302d1774c
emmapstudent <ed.panasiuk@student.uw.edu.pl>
2025-12-17 11:43:31 +0100

    Delete #test.py

--- #test.py
+++
@@ -1,9 +0,0 @@
-#test
-class Animal:
-    def __init__(self, name, species):
-        self.name = name
-        self.species = species
-
-    def make_sound(self):
-        print("Some generic animal sound")
-Animal("Leo", "Lion").make_sound()
